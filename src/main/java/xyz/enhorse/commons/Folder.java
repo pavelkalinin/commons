2f2dc07299f8f8d45c3b12ba2404f05124871f47
pavelkalinin <pavel13kalinin@gmail.com>
2016-07-15 16:27:57 +0500

    Add Folder

---
+++ src/main/java/xyz/enhorse/commons/Folder.java
@@ -0,0 +1,111 @@
+package xyz.enhorse.commons;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static xyz.enhorse.commons.HandyPath.EXTENSION_SEPARATOR;
+
+/**
+ * @author <a href="mailto:pavel13kalinin@gmail.com">Pavel Kalinin</a>
+ *         15.07.2016
+ */
+public class Folder {
+
+    private final File folder;
+
+
+    public Folder(final String directory) {
+        folder = validateDirectory(directory);
+    }
+
+
+    public List<File> getContents(final String... extensions) {
+        return (extensions != null)
+                ? fileList(new FileFilter(extensions))
+                : fileList(null);
+    }
+
+
+    private File validateDirectory(String path) {
+        File result = null;
+
+        if (path != null) {
+            File temp = new File(path);
+            if (!temp.isDirectory()) {
+                result = new File(new HandyPath(temp).pathname());
+            }
+        }
+
+        return result != null
+                ? result
+                : currentDirectory();
+    }
+
+
+    private File folder() {
+        return folder;
+    }
+
+
+    private File currentDirectory() {
+        return new File("");
+    }
+
+
+    private List<File> fileList(FileFilter filter) {
+        List<File> result = new ArrayList<>();
+        HandyPath current = new HandyPath(folder());
+        System.out.println(current);
+        System.out.println(filter);
+
+        try (DirectoryStream<Path> directoryStream = (filter != null)
+                ? Files.newDirectoryStream(current, filter)
+                : Files.newDirectoryStream(current)) {
+            for (Path path : directoryStream) {
+                if (!Files.isDirectory(path))
+                    result.add(path.toFile());
+            }
+        } catch (IOException ex) {
+            throw new IllegalStateException("Listing error on the folder \'" + folder() + '\'', ex);
+        }
+
+        return result;
+    }
+
+
+    private class FileFilter implements DirectoryStream.Filter<Path> {
+
+        private final Set<String> suitable = new HashSet<>();
+
+
+        FileFilter(final String... extensions) {
+            fillSuitable(extensions);
+        }
+
+
+        private void fillSuitable(final String... extensions) {
+            for (String extension : extensions) {
+                extension = extension.trim();
+                if (!extension.isEmpty()) {
+                    if (extension.charAt(0) == EXTENSION_SEPARATOR) {
+                        extension = extension.replace(EXTENSION_SEPARATOR, '\0');
+                    }
+                    suitable.add(extension);
+                }
+            }
+        }
+
+
+        @Override
+        public boolean accept(Path entry) throws IOException {
+            return suitable.contains(new HandyPath(entry).extension());
+        }
+    }
+}
