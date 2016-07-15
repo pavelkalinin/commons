aebd4c0545bb375b6a6fb3c909d2a2725c3f251c
pavelkalinin <pavel13kalinin@gmail.com>
2016-07-15 14:02:59 +0500

    Refactor and extend HandyPath to implement the interface Path

---
+++ src/main/java/xyz/enhorse/commons/HandyPath.java
@@ -0,0 +1,126 @@
+package xyz.enhorse.commons;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * 09.12.2015.
+ */
+public class HandyPath extends AbstractHandyPath {
+
+    public static final char EXTENSION_SEPARATOR = '.';
+    public static final char PATH_SEPARATOR = File.separatorChar;
+
+    private static final String DEFAULT = "";
+
+
+    public HandyPath(final Path path) {
+        super(Validate.defaultIfNull(path, Paths.get(DEFAULT)));
+    }
+
+
+    public HandyPath(final File file) {
+        this(fileToPath(file));
+    }
+
+
+    public HandyPath(String filename) {
+        this(stringToPath(filename));
+    }
+
+
+    private HandyPath(String path, String name, String extension) {
+        this(normalizePathname(path) + name + normalizeExtension(extension));
+    }
+
+
+    public String name() {
+        String filename = toFile().getName();
+        int separator = filename.indexOf(EXTENSION_SEPARATOR);
+
+        return (separator > 0)
+                ? filename.substring(0, separator)
+                : "";
+    }
+
+
+    public String extension() {
+        String filename = toFile().getName();
+        int separator = filename.indexOf(EXTENSION_SEPARATOR);
+
+        return ((separator > 0) && (separator < filename.length() - 1))
+                ? filename.substring(separator + 1)
+                : "";
+    }
+
+
+    public String pathname() {
+        final File parent = toFile().getParentFile();
+
+        return (parent != null)
+                ? parent.getAbsolutePath() + File.separator
+                : "";
+    }
+
+
+    public String filename() {
+        return name() + EXTENSION_SEPARATOR + extension();
+    }
+
+
+    public HandyPath changeName(final String name) {
+        return new HandyPath(pathname(), validate(name), extension());
+    }
+
+
+    public HandyPath changePathname(final String pathname) {
+        return new HandyPath(validate(pathname), name(), extension());
+    }
+
+
+    public HandyPath changeExtension(final String extension) {
+        return new HandyPath(pathname(), name(), validate(extension));
+    }
+
+
+    private static String normalizeExtension(final String extension) {
+        return extension.charAt(0) == EXTENSION_SEPARATOR
+                ? extension
+                : EXTENSION_SEPARATOR + extension;
+    }
+
+
+    private static String normalizePathname(final String path) {
+        return path.charAt(path.length() - 1) == PATH_SEPARATOR
+                ? path
+                : path + PATH_SEPARATOR;
+    }
+
+
+    private static Path stringToPath(final String string) {
+        try {
+            return Paths.get(string);
+        } catch (Exception ex) {
+            return Paths.get(DEFAULT);
+        }
+    }
+
+
+    private static Path fileToPath(final File file) {
+        try {
+            return Validate.notNull("file", file).toPath();
+        } catch (Exception ex) {
+            return Paths.get(DEFAULT);
+        }
+    }
+
+
+    private static String validate(final String string) {
+        try {
+            return Paths.get(string).toString();
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Illegal value \'" + string + '\'');
+        }
+    }
+}
---
+++ src/test/java/xyz/enhorse/commons/HandyPathTest.java
@@ -0,0 +1,191 @@
+package xyz.enhorse.commons;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author <a href="mailto:pavel13kalinin@gmail.com">Pavel Kalinin</a>
+ *         14/07/16
+ */
+public class HandyPathTest {
+
+    private static final String name = "test";
+    private static final String extension = "tst";
+    private static final String filename = name + HandyPath.EXTENSION_SEPARATOR + extension;
+    private static final String weird = "j ::: / _%%%+\'\"??? ;^";
+
+    @Rule
+    public TemporaryFolder tmp = new TemporaryFolder();
+
+
+    private File file() {
+        try {
+            File result = new File(path() + filename);
+            return (result.exists()) ? result : tmp.newFile(name + HandyPath.EXTENSION_SEPARATOR + extension);
+        } catch (IOException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+
+    private String path() {
+        return tmp.getRoot().toString() + HandyPath.PATH_SEPARATOR;
+    }
+
+
+    @Test
+    public void createFromFile() throws Exception {
+        assertNotNull(new HandyPath(file()));
+    }
+
+
+    @Test
+    public void createFromFile_null() throws Exception {
+        File file = null;
+        assertNotNull(new HandyPath(file));
+    }
+
+
+    @Test
+    public void createFromFile_weird() throws Exception {
+        File file = new File(weird);
+        assertNotNull(new HandyPath(file));
+    }
+
+
+    @Test
+    public void createFromPath() throws Exception {
+        assertNotNull(new HandyPath(file().toPath()));
+    }
+
+
+    @Test
+    public void createFromPath_null() throws Exception {
+        Path path = null;
+        assertNotNull(new HandyPath(path));
+    }
+
+
+    @Test
+    public void createFromString() throws Exception {
+        assertNotNull(new HandyPath(file().getAbsoluteFile()));
+    }
+
+
+    @Test
+    public void createFromString_string() throws Exception {
+        String string = null;
+        assertNotNull(new HandyPath(string));
+    }
+
+
+    @Test
+    public void createFromString_weird() throws Exception {
+        assertNotNull(new HandyPath(weird));
+    }
+
+
+    @Test
+    public void pathname() throws Exception {
+        assertEquals(path(), new HandyPath(file()).pathname());
+    }
+
+
+    @Test
+    public void name() throws Exception {
+        assertEquals(name, new HandyPath(file()).name());
+    }
+
+
+    @Test
+    public void extension() throws Exception {
+        assertEquals(extension, new HandyPath(file()).extension());
+    }
+
+
+    @Test
+    public void filename() throws Exception {
+        assertEquals(filename, new HandyPath(file()).filename());
+    }
+
+
+    @Test
+    public void changeName() throws Exception {
+        HandyPath original = new HandyPath(file());
+        String newName = "new" + name;
+        HandyPath changed = original.changeName(newName);
+
+        assertEquals("name wasn't changed", newName, changed.name());
+        assertEquals("immutability was broken", name, original.name());
+    }
+
+
+    @Test
+    public void changeExtension_withoutSeparator() throws Exception {
+        HandyPath original = new HandyPath(file());
+        String newExtension = "new" + extension;
+        HandyPath changed = original.changeExtension(newExtension);
+
+        assertEquals("extension wasn't changed", newExtension, changed.extension());
+        assertEquals("immutability was broken", extension, original.extension());
+    }
+
+
+    @Test
+    public void changePathname_withoutSeparator() throws Exception {
+        HandyPath original = new HandyPath(file());
+        String newPathname = path() + "new";
+        HandyPath changed = original.changePathname(newPathname);
+
+        assertEquals("pathname wasn't changed", newPathname + HandyPath.PATH_SEPARATOR, changed.pathname());
+        assertEquals("immutability was broken", path(), original.pathname());
+    }
+
+
+    @Test
+    public void changeExtension_withSeparator() throws Exception {
+        HandyPath original = new HandyPath(file());
+        String newExtension = HandyPath.EXTENSION_SEPARATOR + "new" + extension;
+        HandyPath changed = original.changeExtension(newExtension);
+
+        assertEquals("extension wasn't changed", newExtension, HandyPath.EXTENSION_SEPARATOR + changed.extension());
+        assertEquals("immutability was broken", extension, original.extension());
+    }
+
+
+    @Test
+    public void changePathname_withSeparator() throws Exception {
+        HandyPath original = new HandyPath(file());
+        String newPathname = path() + "new" + HandyPath.PATH_SEPARATOR;
+        HandyPath changed = original.changePathname(newPathname);
+
+        assertEquals("pathname wasn't changed", newPathname, changed.pathname());
+        assertEquals("immutability was broken", path(), original.pathname());
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void changeName_weird() throws Exception {
+        new HandyPath(file()).changeName(weird);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void changeExtension_weird() throws Exception {
+        new HandyPath(file()).changeExtension(weird);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void changePathname_weird() throws Exception {
+        new HandyPath(file()).changePathname(weird);
+    }
+}
