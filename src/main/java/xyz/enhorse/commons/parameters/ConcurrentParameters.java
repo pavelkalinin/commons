187da5cc04d6f82f28c41d8347b9628502688d9f
pavelkalinin <pavel13kalinin@gmail.com>
2016-09-07 23:26:47 +0500

    Rewrite Parameter interface and its implementations

--- src/main/java/xyz/enhorse/commons/parameters/ConcurrentParameters.java
+++ src/main/java/xyz/enhorse/commons/parameters/ConcurrentParameters.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author <a href="mailto:pavel13kalinin@gmail.com">Pavel Kalinin</a>
  *         27.04.2016
  */
-public class ConcurrentParameters extends AbstractParameters<ConcurrentHashMap> {
+public class ConcurrentParameters extends BasicParameters<ConcurrentHashMap> {
 
     public ConcurrentParameters() {
         super(ConcurrentHashMap.class);
--- src/main/java/xyz/enhorse/commons/parameters/Parameters.java
+++ src/main/java/xyz/enhorse/commons/parameters/Parameters.java
@@ -1,55 +1,14 @@
 package xyz.enhorse.commons.parameters;
 
+import xyz.enhorse.commons.collections.FlexibleBox;
+
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author <a href="mailto:pavel13kalinin@gmail.com">Pavel Kalinin</a>
  *         23.06.2016.
  */
-public interface Parameters extends Iterable<Map.Entry<String, Object>> {
+public interface Parameters extends FlexibleBox<ParameterValue<?>> {
 
-    char PARAMETER_VALUE_SEPARATOR = '=';
-
-
-    Parameters append(Map<String, Object> map);
-
-
-    Parameters add(final String parameter, final Object value);
-
-
-    Parameters put(final String parameter, final Object value);
-
-
-    Parameters replace(final String parameter, final Object newValue);
-
-
-    Parameters remove(final String parameter);
-
-
-    Parameters clear();
-
-
-    Object get(final String parameter);
-
-
-    boolean isExists(final String parameter);
-
-
-    boolean isEmpty();
-
-
-    int size();
-
-
-    List<String> list();
-
-
-    String toString();
-
-
-    Map asMap();
-
-
-    String asURLEncodedString();
+    List<Parameter<?>> list();
 }
